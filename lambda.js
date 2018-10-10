8c4a26b17c66483c91498c30e746765be7fc32f1
Fuchs <granor@localhost.localdomain>
2018-10-10 14:58:53 +0200

    Allow CORS

--- lambda.js
+++ lambda.js
@@ -12,19 +12,25 @@ const easyWash = require('./easyWash.js')
  * DynamoDB API as a JSON body.
  */
 exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('Received event:', JSON.stringify(event, null, 2));
 
     const done = (err, res) => callback(null, {
         statusCode: err ? '400' : '200',
         body: err ? err.message : JSON.stringify(res),
         headers: {
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*'
         },
     });
 
     switch (event.httpMethod) {
         case 'GET':
-            easyWash.getRoom(event.roomId, done)
+            easyWash.getRoom(event.queryStringParameters.roomId, done)
+          break;
+        case 'OPTIONS':
+            //Allow CORS
+            done(null, '');
           break;
         default:
             done(new Error(`Unsupported method "${event.httpMethod}"`));
